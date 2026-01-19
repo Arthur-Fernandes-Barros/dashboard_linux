4942b08b3ce5834bf68010d2102ab2ba6aebb54d
arthur-fernandes-barros <arthurfernandesbarros@gmail.com>
2026-01-19 15:45:26 -0300

    Configuração do ambiente de trabalho e funções iniciais do monitoramento do sistemas

---
+++ main.py
@@ -0,0 +1,11 @@
+import psutil
+
+cpu = psutil.cpu_percent(interval= 1)
+memory = psutil.virtual_memory().percent
+disk = psutil.disk_usage('/').percent
+
+
+print(f'Uso de CPU: {cpu}')
+print(f'Uso de memoria RAM {memory}')
+print(f'Uso de Disko: {disk}')
+
