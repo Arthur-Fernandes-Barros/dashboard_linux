3c0a3402a17f471f5ff1fec29c8e338bafaf7c6d
arthur-fernandes-barros <arthurfernandesbarros@gmail.com>
2026-01-24 19:19:03 -0300

    formatação documentos .py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import psutil
 
-cpu = psutil.cpu_percent(interval= 1)
+cpu = psutil.cpu_percent(interval=1)
 memory = psutil.virtual_memory().percent
-disk = psutil.disk_usage('/').percent
+disk = psutil.disk_usage("/").percent
 
 
-print(f'Uso de CPU: {cpu}')
-print(f'Uso de Memoria RAM {memory}')
-print(f'Uso de Disko: {disk}')
+print(f"Uso de CPU: {cpu}")
+print(f"Uso de Memoria RAM {memory}")
+print(f"Uso de Disko: {disk}")
