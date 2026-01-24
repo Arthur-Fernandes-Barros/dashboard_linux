043b1824790b26049d5234973c35fa2058df5787
arthur-fernandes-barros <arthurfernandesbarros@gmail.com>
2026-01-24 20:13:57 -0300

    Finalização base interface grafica, e adição da função rerun para atualizar automaticamente

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
+import streamlit as st
 import psutil
+import time
 
-cpu = psutil.cpu_percent(interval=1)
-memory = psutil.virtual_memory().percent
+st.set_page_config(layout="wide")
+
+st.title("Linux System Dashboard")
+
+cpu = psutil.cpu_percent()
+ram = psutil.virtual_memory().percent
 disk = psutil.disk_usage("/").percent
 
+col1, col2, col3 = st.columns(3)
+col1.metric("CPU", f"{cpu}%")
+col2.metric("RAM", f"{ram}%")
+col3.metric("Disco", f"{disk}%")
 
-print(f"Uso de CPU: {cpu}")
-print(f"Uso de Memoria RAM {memory}")
-print(f"Uso de Disko: {disk}")
+time.sleep(2)
+st.rerun()
